c37d425a3d03a47584008c5e976781cf1c61942f
Chris5477 <petitcolaschristopher@gmail.com>
2022-07-26 22:58:01 +0200

    add fade picture in login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,9 @@
 import logoVP from "../assets/logo/VPLogo.svg";
 import bgLogin from "../assets/background-image/bg-login.jpg";
 import Input from "../components/Input";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import bgLogin2 from "../assets/background-image/bg-login2.jpg";
+import bgLogin3 from "../assets/background-image/bg-login3.jpg";
 
 const Login = () => {
 	const loginInitialState = {
@@ -10,6 +12,14 @@ const Login = () => {
 		email: "",
 	};
 
+	const listBgImage = [
+		{ picture: bgLogin, alt: "paysage avec des montgolfière" },
+		{ picture: bgLogin2, alt: "vue sur des grattes-ciel" },
+		{ picture: bgLogin3, alt: "ville au bord de la mer" },
+	];
+
+	const [a, setA] = useState(0);
+	const [b, setB] = useState(1);
 	const [userData, setUserData] = useState(loginInitialState);
 	const [messageForm, setMessageForm] = useState(false);
 	const { firstName, lastName, email } = userData;
@@ -33,6 +43,39 @@ const Login = () => {
 		setTimeout(() => (window.location.href = "localhost:3000/offers"), 2000);
 	};
 
+	const removeClassCSS = () => {
+		document.querySelector(".bg-login img").classList.remove("fade-out");
+		document.querySelector(".bg-login img:last-child").classList.remove("fade-in");
+	};
+
+	const addAnimationClass = () => {
+		document.querySelector(".bg-login img").classList.add("fade-out");
+		document.querySelector(".bg-login img:last-child").classList.add("fade-in");
+	};
+
+	const settingsIndex = () => {
+		const c = a + 1;
+		const d = b + 1;
+		if (listBgImage.at(c) == undefined) {
+			setA(0);
+			setB(d);
+		} else if (listBgImage.at(d) == undefined) {
+			setB(0);
+			setA(c);
+		} else {
+			setA(c);
+			setB(d);
+		}
+	};
+
+	useEffect(() => {
+		setTimeout(() => {
+			settingsIndex();
+			removeClassCSS();
+			setTimeout(addAnimationClass, 100);
+		}, 5000);
+	}, [a, b]);
+
 	const login = (e) => {
 		e.preventDefault();
 		const valueState = Object.values(userData).every((value) => value);
@@ -48,7 +91,8 @@ const Login = () => {
 	return (
 		<div className="page login-page">
 			<figure className="bg-login">
-				<img src={bgLogin} alt="paysage avec des montgolfière" />
+				<img className="banner" src={listBgImage[a].picture} alt={listBgImage[a].alt} />
+				<img className="banner absolute-pos" src={listBgImage[b].picture} alt={listBgImage[b].alt} />
 			</figure>
 			<header>
 				<h1>
