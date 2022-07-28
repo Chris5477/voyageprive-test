24e775caf1541f23356f7e18b4b41d73e03d6aa2
Chris5477 <petitcolaschristopher@gmail.com>
2022-07-28 14:38:29 +0200

    convert format jpg to webp to improve performances

--- src/utils/listPicturesBg.js
+++ src/utils/listPicturesBg.js
@@ -1,6 +1,6 @@
-import bgLogin from "../assets/background-image/bg-login.jpg";
-import bgLogin2 from "../assets/background-image/bg-login2.jpg";
-import bgLogin3 from "../assets/background-image/bg-login3.jpg";
+import bgLogin from "../assets/background-image/bg-login.webp";
+import bgLogin2 from "../assets/background-image/bg-login2.webp";
+import bgLogin3 from "../assets/background-image/bg-login3.webp";
 
 export const listBgImage = [
 	{ picture: bgLogin, alt: "paysage avec des montgolfière" },
